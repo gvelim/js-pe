af7957f398d398857916f5efa22633c00cb3e62c
gvelim <3185416+gvelim@users.noreply.github.com>
2024-04-19 23:01:04 +0100

    adding bun server

--- index.ts
+++ index.ts
@@ -1,28 +1,31 @@
 import { parseArgs } from "util";
 
 console.log(Bun.argv);
-const { values, positionals } = parseArgs({
+const { values } = parseArgs({
   args: Bun.argv,
   options: {
     file: {
       type: "string",
+      short: "f",
     },
   },
   strict: true,
   allowPositionals: true,
 });
 
+let base = values.file?.slice(0, values.file?.lastIndexOf("/"));
+
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    let url = new URL(req.url).pathname;
-    console.log(req);
-    let res =
-      url === "/"
-        ? new Response(Bun.file(values.file as string))
-        : new Response(Bun.file("." + url));
-    console.log(res);
-    return res;
+    let path = new URL(req.url).pathname;
+    return new Response(
+      path === "/" ? Bun.file(values.file as string) : Bun.file(base + path),
+    );
+  },
+  error(request) {
+    console.log(request);
+    return new Response("Cought Error: " + request);
   },
 });
 
